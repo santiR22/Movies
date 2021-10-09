05675eabbd012b0f1dcab83286ae589efaf2ed5a
santiR22 <santiagorodas688@gmail.com>
2021-10-09 11:45:56 -0300

    first commit

---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,18 @@
+export function MoviesGrid() {
+    return(
+        
+        <div>
+            <header>
+                <h1>Movies</h1>
+            </header>
+            <main>
+                <ul>
+                    <li>Peli1</li>
+                    <li>Peli2</li>
+                    <li>Peli3</li>
+                </ul>
+            </main>
+        </div>
+
+    )
+}
